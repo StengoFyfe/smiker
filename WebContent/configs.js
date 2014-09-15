d3fcc92ce684cc0f9bee7405c67af4d35c0391d8
Frank Dietrich <frank.dietrich@pta.eu>
2014-09-15 16:07:01 +0200

    one more Comment

--- WebContent/configs.js
+++ WebContent/configs.js
@@ -2,6 +2,7 @@
 var isSmallMode = false;
 var isLandscape = false;
 
+// Comment only for B1.0.0_Firsty
 function convertDateStringsToDates(input) {
     // Ignore things that aren't objects.
     if (typeof input !== "object") return input;
